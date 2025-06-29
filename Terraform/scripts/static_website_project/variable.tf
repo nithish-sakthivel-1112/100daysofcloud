652f652894d9ca5972ff1073aede84bb1710496f
nithish-sakthivel-1112 <nithish0829@gmail.com>
2025-06-29 12:28:09 +0530

    Completed the Static website project

---
+++ Terraform/scripts/static_website_project/variable.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "bucket_name" {
+  default = "nithish-static-website"
+}
+
+variable "website_index_document" {
+  default = "index.html"
+}
+
+variable "website_error_document" {
+  default = "error.html"
+}
